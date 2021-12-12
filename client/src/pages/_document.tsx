b95f366a0d2e91f46961c7264dd13c698e7e0fd9
Jomark Pangan <jomarkrazonpangan@gmail.com>
2021-12-12 22:36:05 +0800

    changed font to IBM Plex Sans

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -12,6 +12,8 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet" />
         </Head>
         <body>
