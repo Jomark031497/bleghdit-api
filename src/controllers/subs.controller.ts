6e01a902d74cc00055e45c4633b627bc4aaf0ffa
Jomark Pangan <jomarkrazonpangan@gmail.com>
2021-12-17 01:06:11 +0800

    added endpoint: delete sub images and fixed deleting old images error by changing \ (windows) to / (linux)

--- src/controllers/subs.controller.ts
+++ src/controllers/subs.controller.ts
@@ -67,24 +67,57 @@ export const getSub = async (req: Request, res: Response) => {
 
 export const uploadSubImage = async (req: Request, res: Response) => {
   const user: any = req.user; // get the user from session
+  const sub: Subs = user.sub;
   const type = req.body.type; // get the type (image/bannner) from req.body
-  const file: any = req.file; // get file from req.file
   try {
     // return an error if the type isn't image or banner
     if (type !== "image" && type !== "banner") {
-      fs.unlinkSync(file.path); // delete the photo
+      fs.unlinkSync(req.file!.path); // delete the photo
       return res.status(400).json({ error: "invalid type" }); // return an error
     }
 
-    // get the urn from the req.file
-    const urn: any = file.filename;
+    let oldImageUrn: string = ""; // store the old image here
 
-    if (type === "image") user.sub.imageURN = urn;
-    else user.sub.bannerURN = urn;
+    if (type === "image") {
+      oldImageUrn = sub.imageURN || ""; // store the old imageURN
+      sub.imageURN = req.file!.filename; // add the imageURN to the sub
+    } else if (type === "banner") {
+      oldImageUrn = sub.bannerURN || ""; // store the old bannerURN
+      sub.bannerURN = req.file!.filename; // add the new banner URN
+    }
 
-    await user.sub.save();
+    await sub.save();
 
-    return res.status(200).json(user.sub);
+    if (oldImageUrn !== "") fs.unlinkSync(`public/images/${oldImageUrn}`); // filesystem made  me up lmao, windows = \\ ; linux = /
+
+    return res.status(200).json(sub);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "something went wrong" });
+  }
+};
+
+export const deleteSubImage = async (req: Request, res: Response) => {
+  const { name, type } = req.params;
+  try {
+    if (type !== "image" && type !== "banner") {
+      return res.status(400).json({ error: "invalid type" }); // return an error
+    }
+
+    const sub = await Subs.findOneOrFail({ name });
+
+    if (type === "image") {
+      // delete
+
+      sub.imageURN = "";
+    } else if (type === "banner") {
+      // delete
+      sub.bannerURN = "";
+    }
+
+    await sub.save();
+
+    return res.status(200).json(sub);
   } catch (error) {
     return res.status(500).json({ error: "something went wrong" });
   }
--- src/routes/subs.routes.ts
+++ src/routes/subs.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createSub, getSub, uploadSubImage } from "../controllers/subs.controller";
+import { createSub, deleteSubImage, getSub, uploadSubImage } from "../controllers/subs.controller";
 import requireAuth from "../middlewares/requireAuth";
 import { userOwnsSub, upload } from "../middlewares/uploadImage";
 
@@ -10,5 +10,7 @@ router.post("/create", requireAuth, createSub);
 router.get("/:name", getSub);
 
 router.post("/:name/image", requireAuth, userOwnsSub, upload.single("file"), uploadSubImage);
+
+router.delete("/:name/:type", requireAuth, userOwnsSub, deleteSubImage);
 
 export default router;
