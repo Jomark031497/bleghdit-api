b8d32456a4c86ab1e9ca07439c3b4a508181af91
Jomark Pangan <jomarkrazonpangan@gmail.com>
2021-12-26 11:44:24 +0800

    removed frontpage actions

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,7 +4,6 @@ import Head from "next/head";
 import { Box, Container } from "@mui/material";
 import { Post } from "../types";
 import PostCard from "../components/PostCard";
-import FrontPageActions from "../components/FrontPageActions";
 import FrontPageSideBar from "../components/FrontPageSideBar";
 
 const Home: NextPage = () => {
@@ -18,8 +17,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Container maxWidth="lg" sx={{ py: "3rem", display: "flex" }}>
-        <Box sx={{ flex: 1 }}>
-          <FrontPageActions />
+        <Box sx={{ flex: 1, mt: "0.5rem" }}>
           {posts && posts.map((post: Post) => <PostCard post={post} key={post.identifier} />)}
         </Box>
         <Box sx={{ flex: 0.4 }}>
