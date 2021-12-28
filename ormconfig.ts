f1539eb0dd063e3fdf6e7f56dfd44695857d559e
Jomark Pangan <jomarkrazonpangan@gmail.com>
2021-12-28 13:48:58 +0800

    added ormconfig

---
+++ ormconfig.ts
@@ -0,0 +1,33 @@
+const prodConfig = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  logging: true,
+  entities: ["src/entities/**/*.ts"],
+  migrations: ["src/migrations/**/*.ts"],
+  subscribers: ["src/subscribers/**/*.ts"],
+};
+
+const devConfig = {
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: true,
+  entities: ["src/entities/**/*.ts"],
+  migrations: ["src/migrations/**/*.ts"],
+  subscribers: ["src/subscribers/**/*.ts"],
+  seeds: ["src/seeds/**/*{.ts,.js}"],
+  cli: {
+    entitiesDir: "src/entities",
+    migrationsDir: "src/migrations",
+    subscribersDir: "src/subscriber",
+  },
+};
+
+const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+
+module.exports = config;
