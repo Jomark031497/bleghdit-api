5baf9ef91b364dd990d4d7f00da644e8e739f236
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-01-01 10:46:25 +0800

    fixed ormconfig still ponting to dev

--- ormconfig.ts
+++ ormconfig.ts
@@ -23,14 +23,14 @@ const devConfig = {
   database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
-  entities: ["src/entities/**/*.ts"],
-  migrations: ["src/migrations/**/*.ts"],
-  subscribers: ["src/subscribers/**/*.ts"],
-  seeds: ["src/seeds/**/*{.ts,.js}"],
+  entities: ["dist/entities/**/*.js"],
+  migrations: ["dist/migrations/**/*.js"],
+  subscribers: ["dist/subscribers/**/*.js"],
+  seeds: ["dist/seeds/**/*{.ts,.js}"],
   cli: {
-    entitiesDir: "src/entities",
-    migrationsDir: "src/migrations",
-    subscribersDir: "src/subscriber",
+    entitiesDir: "dist/entities",
+    migrationsDir: "dist/migrations",
+    subscribersDir: "dist/subscriber",
   },
 };
 
