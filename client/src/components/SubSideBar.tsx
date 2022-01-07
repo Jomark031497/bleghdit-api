cbb5349a3d65c0f4291c74c640bae781bb7e28fd
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-01-07 22:21:33 +0800

    Removed online count and subscribers count

--- client/src/components/SubSideBar.tsx
+++ client/src/components/SubSideBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button, Avatar, Snackbar, IconButton } from "@mui/material";
+import { Box, Typography, Button, Avatar, Snackbar, IconButton, Divider } from "@mui/material";
 import { Sub } from "../types";
 import CakeIcon from "@mui/icons-material/Cake";
 import dayjs from "dayjs";
@@ -40,19 +40,21 @@ const SubSideBar: React.FC<SubProp> = ({ sub }) => {
       </Box>
 
       <Box sx={{ p: "0.5rem" }}>
-        <Box sx={{ display: "flex", alignItems: "center", my: "0.5rem" }}>
-          <Avatar src={sub.imageURN} />
-          <CLink
-            href={`/r/${sub.name}`}
-            variant="h6"
-            color="textPrimary"
-            label={`r/${sub.name}`}
-            sx={{ mx: "0.3rem" }}
-          />
+        <Box sx={{ pb: "1rem" }}>
+          <Box sx={{ display: "flex", alignItems: "center", my: "0.5rem" }}>
+            <Avatar src={sub.imageURN} />
+            <CLink
+              href={`/r/${sub.name}`}
+              variant="h6"
+              color="textPrimary"
+              label={`r/${sub.name}`}
+              sx={{ mx: "0.3rem" }}
+            />
+          </Box>
+          <Typography variant="subtitle2">{sub.description}</Typography>
         </Box>
-        <Typography variant="subtitle2">{sub.description}</Typography>
-        <hr />
-        <Box sx={{ display: "flex" }}>
+        <Divider />
+        {/* <Box sx={{ display: "flex" }}>
           <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", m: "0.5rem" }}>
             <Typography variant="subtitle1">18.5k</Typography>
             <Typography variant="subtitle2">Evergrow Army</Typography>
@@ -61,7 +63,7 @@ const SubSideBar: React.FC<SubProp> = ({ sub }) => {
             <Typography variant="subtitle1">109</Typography>
             <Typography variant="subtitle2">Online</Typography>
           </Box>
-        </Box>
+        </Box> */}
         <Box sx={{ display: "flex", alignItems: "center", my: "1rem" }}>
           <CakeIcon />
           <Typography>Created {dayjs(sub.createdAt).format("D MMM YYYY")}</Typography>
