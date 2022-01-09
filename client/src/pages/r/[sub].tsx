81fddd6b2112b42a63faf3642918f4d66b76c492
Jomark Pangan <jomarkrazonpangan@gmail.com>
2022-01-09 19:01:51 +0800

    added floating action button

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, SpeedDial, SpeedDialAction, styled } from "@mui/material";
 import { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -9,10 +9,27 @@ import SubHeader from "../../components/SubHeader";
 import SubSideBar from "../../components/SubSideBar";
 import { Post, Sub } from "../../types";
 
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+
+import CreatePost from "@mui/icons-material/Create";
+
 const Subleddit: NextPage = () => {
   const router = useRouter();
 
   const { data: sub } = useSWR<Sub>(router.query.sub ? `/subs/${router.query.sub}` : null);
+  const actions = [
+    {
+      icon: <CreatePost />,
+      name: "Create Post",
+      action: {},
+    },
+  ];
+
+  const handleClick = (operation: string) => {
+    if (operation === "Create Post") {
+      router.push(`/r/${router.query.sub}/create`);
+    }
+  };
 
   return (
     <>
@@ -26,25 +43,49 @@ const Subleddit: NextPage = () => {
       {sub && (
         <>
           <SubHeader sub={sub} />
-          <Container
-            id="main-container"
-            maxWidth="lg"
-            sx={{ background: "transparent", pb: "1rem", display: "flex", flexDirection: { xs: "column", md: "row" } }}
-          >
-            <Box id="main-content" sx={{ flex: 1, order: { xs: 2, md: 1 }, my: "1rem" }}>
+          <Container id="main-container" maxWidth="lg" sx={{ background: "transparent", pb: "1rem", display: "flex" }}>
+            <Box id="main-content" sx={{ flex: 1, my: "1rem" }}>
               {sub.posts.map((post: Post) => (
                 <PostCard post={post} key={post.identifier} />
               ))}
             </Box>
 
-            <Box id="sidebar" sx={{ flex: 0.4, ml: { xs: 0, md: "1rem" }, order: { xs: 1, md: 2 }, my: "1rem" }}>
+            <Box
+              id="sidebar"
+              sx={{ flex: 0.4, ml: { xs: 0, md: "1rem" }, my: "1rem", display: { xs: "none", md: "block" } }}
+            >
               <SubSideBar sub={sub} />
             </Box>
           </Container>
+          <Box sx={{ display: { xs: "block", md: "none" }, position: "relative", mt: 3, height: 320 }}>
+            <StyledSpeedDial ariaLabel="speed dial" icon={<SpeedDialIcon />} direction="up">
+              {actions.map((action) => (
+                <SpeedDialAction
+                  key={action.name}
+                  icon={action.icon}
+                  tooltipTitle={action.name}
+                  onClick={() => handleClick(action.name)}
+                  tooltipOpen
+                />
+              ))}
+            </StyledSpeedDial>
+          </Box>
         </>
       )}
     </>
   );
 };
+
+const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
+  position: "fixed",
+  "&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft": {
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  "&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight": {
+    top: theme.spacing(2),
+    left: theme.spacing(2),
+  },
+}));
 
 export default Subleddit;
